5abc6596370b50c27c4970871d544f4ad4e16a0b
VictorSandbox <victor.w.cairo@gmail.com>
2021-07-25 19:10:23 -0500

    intial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,101 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HoustonAstros class="experiment" :players="Astrosplayers" />
+    <NYYankees :players="NYplayers"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HoustonAstros from "./components/HoustonAstros.vue";
+import NYYankees from "./components/NYYankees.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HoustonAstros,
+    NYYankees,
+  },
+
+  data() {
+    return {
+      Astrosplayers: [
+        {
+          number: "27",
+          name: "Jose Altuve",
+          position: "2B",
+          nationality: "Venezuela",
+        },
+        {
+          number: "23",
+          name: "Michael Brantley",
+          position: "LF",
+          nationality: "American",
+        },
+        {
+          number: "2",
+          name: "Alex Bregman",
+          position: "3B",
+          nationality: "American",
+        },
+        {
+          number: "44",
+          name: "Yordan Alvarez",
+          position: "DH",
+          nationality: "Cuba",
+        },
+        {
+          number: "10",
+          name: "Yuli Gurriel",
+          position: "1B",
+          nationality: "Cuba",
+        },
+      ],
+
+      NYplayers: [
+        {
+          number: "26",
+          name: "DJ leMahieu",
+          position: "1B",
+          nationality: "American",
+        },
+        {
+          number: "27",
+          name: "Giancarlo Stanton",
+          position: "DH",
+          nationality: "American",
+        },
+        {
+          number: "2",
+          name: "Aaron Judge",
+          position: "RF",
+          nationality: "American",
+        },
+        {
+          number: "29",
+          name: "Gio Urshela",
+          position: "3B",
+          nationality: "American",
+        },
+        {
+          number: "25",
+          name: "Gleiber Torres",
+          position: "SS",
+          nationality: "Republica Dominicana",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    display:flex;
+  }
+
+
+
+
+
+
 </style>
---
+++ src/components/NYYankees.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="player-list">
+    <h2>NewYork Yankees Rosters</h2>
+    <!--  <h3>Batters</h3> -->
+    <!-- <ul>
+      <li></li>
+    </ul> -->
+    <!-- <h3>Pitchers</h3> -->
+    <ul v-for="player in players" :key="player.number">
+      <li>
+        <p>{{ player.name }}</p>
+        <p>{{ player.position }}</p>
+        <p>{{ player.nationality }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NYYankees",
+  props: ["players"],
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  display: block;
+}
+
+.player-list {
+  background: lightskyblue;
+  padding: 10px 20px;
+  /* height: 75px; */
+  /* width: 400px; */
+  margin: 10px;
+}
+
+.player-list ul {
+  list-style-type: none;
+}
+
+ul li {
+  text-align: left;
+}
+
+li p {
+  display: inline-block;
+  padding-right: 15px;
+  margin: 0px;
+}
+</style>
